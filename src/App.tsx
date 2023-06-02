002f914bddcd7fcf63e0a85e1088b0b72a2582c3
IJTINK7 <ijtink7@mail.ru>
2023-06-02 09:24:28 +0300

    Preparing for the lesson

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,46 @@
-// EASY LEVEL
-
+// // EASY LEVEL
 import React, {useState} from 'react';
 import './App.css';
-import {Todolist} from './Todolist';
+import {WishList} from './WishList';
 import {v1} from 'uuid';
 
 function App() {
 
-  let [tasks, setTasks] = useState([
-    {id: v1(), title: "HTML&CSS", isDone: true},
-    {id: v1(), title: "JS", isDone: true},
-    {id: v1(), title: "ReactJS", isDone: false},
-    {id: v1(), title: "Rest API", isDone: false},
-    {id: v1(), title: "GraphQL", isDone: false},
-  ]);
+	let [tasks, setTasks] = useState([
+		{id: v1(), title: "HTML&CSS", isDone: true},
+		{id: v1(), title: "JS", isDone: true},
+		{id: v1(), title: "ReactJS", isDone: false},
+		{id: v1(), title: "Rest API", isDone: false},
+		{id: v1(), title: "GraphQL", isDone: false},
+	]);
 
-  function removeTask(taskID: string) {
-    //setTasks()
-  }
+	function removeTask(taskID: string) {
+		//setTasks()
+	}
 
-  function changeTaskStatus(taskId: string, isDone: boolean) {
-    //setTasks()
-  }
+	function changeTaskStatus(taskId: string, isDone: boolean) {
+		//setTasks()
+	}
 
-  return (
-      <div className="App">
-        <Todolist title="What to learn"
-                  tasks={tasks}
-                  removeTask={removeTask}
-                  changeTaskStatus={changeTaskStatus}
-        />
-      </div>
-  );
+	return (
+		<div className="App">
+			<WishList title="What to learn"
+					  tasks={tasks}
+					  removeTask={removeTask}
+					  changeTaskStatus={changeTaskStatus}
+			/>
+		</div>
+	);
 }
 
 export default App;
 
 
-// MEDIUM LEVEL
-//
+// // MEDIUM LEVEL
+
 // import React, {useState} from 'react';
 // import './App.css';
-// import {TaskType, Todolist} from './Todolist';
+// import {TaskType, WishList} from './WishList';
 // import {v1} from 'uuid';
 //
 // export type FilterValuesType = "all" | "active" | "completed";
@@ -94,7 +93,7 @@ export default App;
 //       <div className="App">
 //         {todolists.map(el => {
 //           let tasksForTodolist = tasks[el.id];
-//           return <Todolist
+//           return <WishList
 //               key={el.id}
 //               todolistID={el.id}
 //               title={el.title}
@@ -109,11 +108,11 @@ export default App;
 //
 // export default App;
 
-// HARD LEVEL
-//
+// // HARD LEVEL
+
 // import React, {useState} from 'react';
 // import './App.css';
-// import {TaskType, Todolist} from './Todolist';
+// import {TaskType, WishList} from './WishList';
 // import {v1} from 'uuid';
 //
 // export type FilterValuesType = "all" | "active" | "completed";
@@ -182,7 +181,7 @@ export default App;
 //         {todolists.map((el) => {
 //           let tasksForTodolist = tasks[el.id].entryPoint.data;
 //           return (
-//               <Todolist
+//               <WishList
 //                   key={el.id}
 //                   todolistID={el.id}
 //                   title={el.title}
@@ -194,6 +193,81 @@ export default App;
 //         })}
 //       </div>
 //   );
+// }
+//
+// export default App;
+
+
+// WishList 07
+//
+// import React, {useState} from 'react';
+// import './App.css';
+// import {OsTypeForSelect, StatusTypeForSelect, WishList} from "./WishList";
+// import {v1} from "uuid";
+//
+// export type OsType = "All" | "iOS" | "Android" | OsTypeForSelect
+//
+// export type WishesDataPropsType = {
+// 	id: string
+// 	title: string
+// 	OS: OsType
+// 	checked: boolean
+// }
+//
+// function App() {
+// 	const [activityFilter, setActivityFilter] = useState<StatusTypeForSelect>("All")
+// 	const [osFilter, setOsFilter] = useState<OsType>("All")
+// 	const [wishes, setWishes] = useState<WishesDataPropsType[]>([
+// 		{id: v1(), title: 'Samsung Galaxy S23', OS: "Android", checked: true},
+// 		{id: v1(), title: 'IPhone 13 ProMax', OS: "iOS", checked: true},
+// 		{id: v1(), title: 'Xiaomi 13', OS: "Android", checked: true},
+// 		{id: v1(), title: 'Huawei', OS: "Android", checked: false},
+// 		{id: v1(), title: 'Iphone 14', OS: "iOS", checked: false},
+// 	])
+//
+// 	const addNewWish = (oS: OsTypeForSelect, newValue: string) => {
+// 		setWishes([{id: v1(), title: newValue, OS: oS, checked: false}, ...wishes])
+// 	}
+// 	const removeWish = (id: string) => {
+// 		setWishes(wishes.filter(el => el.id !== id))
+// 	}
+//
+// 	//{id: v1(), title: 'Samsung Galaxy S23', OS: "Android", checked: true, isTrue: statusValue }
+// 	//1. Видишь объект-делай копию. 2. Видишь массив-делай копию. 3. Видишь ключ-создавай новый.
+//
+// 	const changeWishStatus = (wishId: string, statusValue: boolean) => {
+// 		setWishes(wishes.map((el)=> el.id === wishId ? {...el, checked: statusValue} :el))
+// 	}
+//
+//
+//
+//
+// 	const wishesWhatWeWantToSee = (osFilter === "All" ? wishes : wishes.filter(el => el.OS === osFilter)) // select OS
+//
+//
+// 	const wishesWhatWeWantToSeeGeneral = activityFilter === "All" ?
+// 		wishesWhatWeWantToSee : activityFilter === "Active" ?
+// 		wishesWhatWeWantToSee.filter(el=> !el.checked )
+// 		: wishesWhatWeWantToSee.filter(el=> el.checked )
+// 	// select Activity
+//
+//
+//
+// 	return (
+// 		<div className="App">
+// 			<WishList wishes={wishesWhatWeWantToSeeGeneral}
+// 					  addNewWish={addNewWish}
+// 					  osFilter={osFilter}
+// 					  setOsFilter={setOsFilter}
+// 					  removeWish={removeWish}
+// 					  activityFilter={activityFilter}
+// 					  setActivityFilter={setActivityFilter}
+// 					  changeWishStatus={changeWishStatus}
+//
+//
+// 			/>
+// 		</div>
+// 	);
 // }
 //
 // export default App;
