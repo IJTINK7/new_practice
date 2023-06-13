927fa2bcee8b898c435bbb516077dcd8bcc77642
IJTINK7 <ijtink7@mail.ru>
2023-06-13 16:24:28 +0300

    Changing typification for actions

--- src/state/todolists-reducer.ts
+++ src/state/todolists-reducer.ts
@@ -1,16 +1,30 @@
-import {TodolistType} from "../App";
+import {FilterValuesType, TodolistType} from "../App";
 import {v1} from "uuid";
 
-type ActionType = {
-	type: string
-	[key: string]: any
+type ActionsType = RemoveTodolistActionType | AddTodolistActionType | ChangeTodolistTitleActionType |ChangeTodolistFilterActionType
+type RemoveTodolistActionType={
+	type: 'REMOVE-TODOLIST'
+	id: string
 }
-
+type AddTodolistActionType={
+	type: 'ADD-TODOLIST'
+	title: string
+}
+type ChangeTodolistTitleActionType={
+	type: 'CHANGE-TODOLIST-TITLE'
+	id: string
+	title: string
+}
+type ChangeTodolistFilterActionType={
+	type: 'CHANGE-TODOLIST-FILTER'
+	id: string
+	filter: FilterValuesType
+}
 // меня вызовут и дадут мне стейт (почти всегда объект)
 // и инструкцию (action, тоже объект)
 // согласно прописанному type в этом action (инструкции) я поменяю state
 
-export const todolistsReducer = (state: TodolistType[], action: ActionType): TodolistType[] => {
+export const todolistsReducer = (state: TodolistType[], action: ActionsType): TodolistType[] => {
 	switch (action.type) {
 		case 'REMOVE-TODOLIST':
 			return state.filter(el=>el.id !== action.id)
