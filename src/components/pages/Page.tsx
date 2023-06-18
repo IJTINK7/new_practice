41ce33c4854a19ea049616f9722b7316d9211458
IJTINK7 <ijtink7@mail.ru>
2023-06-19 00:48:37 +0300

    Solving the problem with a wrong path of Navigate component(if number of :id will be greater than data array length)

--- src/components/pages/Page.tsx
+++ src/components/pages/Page.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {PagesType} from "../dataState/dataState";
-import {useParams} from "react-router-dom";
+import {Route, Routes, useParams} from "react-router-dom";
+import {Error404} from "./Error404";
 
 type PropsType = {
 	pages: PagesType[]
@@ -8,10 +9,18 @@ type PropsType = {
 export const Page = (props: PropsType) => {
 	const params = useParams();
 	console.log(params)
+	if (Number(params.stolenID) <= Number(props.pages.length)) {
 		return (
 			<div>
 				<div>Heading: {props.pages[Number(params.stolenID)].heading}</div>
 				<div>About: {props.pages[Number(params.stolenID)].about}</div>
 			</div>
+
 		);
+	}else
+		return (
+			<Routes>
+				<Route path={"/*"} element={<Error404/>}/>
+			</Routes>
+		)
 };
