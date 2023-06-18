c009c16b448057b94aa05b8a506b769234ff7c2d
IJTINK7 <ijtink7@mail.ru>
2023-06-19 00:46:56 +0300

    Showing a content depending on element of data Array

--- src/components/pages/Page.tsx
+++ src/components/pages/Page.tsx
@@ -2,16 +2,16 @@ import React from 'react';
 import {PagesType} from "../dataState/dataState";
 import {useParams} from "react-router-dom";
 
-
 type PropsType = {
 	pages: PagesType[]
 }
 export const Page = (props: PropsType) => {
 	const params = useParams();
 	console.log(params)
-	return (
-		<div>
-			hi
-		</div>
-	);
+		return (
+			<div>
+				<div>Heading: {props.pages[Number(params.stolenID)].heading}</div>
+				<div>About: {props.pages[Number(params.stolenID)].about}</div>
+			</div>
+		);
 };
