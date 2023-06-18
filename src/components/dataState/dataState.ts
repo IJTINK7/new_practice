b947e5a4c1f991b4d66bde014b6bb1c2bd549b23
IJTINK7 <ijtink7@mail.ru>
2023-06-18 23:02:41 +0300

    Creating dataState.ts file

---
+++ src/components/dataState/dataState.ts
@@ -0,0 +1,25 @@
+export const dataState: DataStateType = {
+	pages: [
+		{
+			heading: 1,
+			about: 111,
+		},
+		{
+			heading: 2,
+			about: 222,
+		},
+		{
+			heading: 3,
+			about: 333,
+		},
+	],
+}
+
+export type DataStateType={
+	pages: PagesType[]
+}
+
+export type PagesType={
+	heading: number
+	about: number
+}
