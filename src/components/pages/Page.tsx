e2f229e40c79881e024a4239070b4edadcd95485
IJTINK7 <ijtink7@mail.ru>
2023-06-19 06:14:15 +0300

    Changing the showing of necessary components with their data depending on :id in address bar

--- src/components/pages/Page.tsx
+++ src/components/pages/Page.tsx
@@ -1,27 +1,21 @@
 import React from 'react';
 import {PagesType} from "../dataState/dataState";
-import {Route, Routes, useParams} from "react-router-dom";
+import {useParams} from "react-router-dom";
 import {Error404} from "./Error404";
+import {Content} from "../Content";
+import {HomePage} from "../HomePage";
 
 type PropsType = {
 	pages: PagesType[]
 }
 export const Page = (props: PropsType) => {
 	const params = useParams();
-	console.log(isNaN(Number(params.stolenID)))
-	console.log(Number(params.stolenID))
-	if (!isNaN(Number(params.stolenID)) && Number(params.stolenID) < Number(props.pages.length)) {
-		return (
-			<div>
-				<div>Heading: {props.pages[Number(params.stolenID)].heading}</div>
-				<div>About: {props.pages[Number(params.stolenID)].about}</div>
-			</div>
-
-		);
-	}else
-		return (
-			<Routes>
-				<Route path={"/*"} element={<Error404/>}/>
-			</Routes>
-		)
+	return(
+		Number(params.stolenID) === 0
+			? <HomePage heading={props.pages[Number(params.stolenID)].heading} about={props.pages[Number(params.stolenID)].about}/>
+			:
+		props.pages[Number(params.stolenID)]
+			? <Content heading={props.pages[Number(params.stolenID)].heading} about={props.pages[Number(params.stolenID)].about}/>
+			: <Error404/>
+	);
 };
